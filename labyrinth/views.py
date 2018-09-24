887de34a9fd076883a56f82637822ccab24e6cac
IsaackHazaell <isaachvc@hotmail.com>
2018-09-24 16:57:24 -0500

    Se añadio la segunda vista con sus respetivas rutas, aún falta terminar

--- labyrinth/views.py
+++ labyrinth/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 
 def index(request):
     return render_to_response('landingPage/index.html')
+
+def upload(request):
+    return render_to_response('landingPage/upload.html')

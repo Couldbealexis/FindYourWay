aa48cc8f5a6c53a372eccc1e22bb73c2984aa876
couldbealexis <alexis.gutierrez@alumno.udg.mx>
2018-09-29 12:40:09 -0500

    added support for sweet alert

--- static/js/labyrinth/preview.js
+++ static/js/labyrinth/preview.js
@@ -1,5 +1,7 @@
+
 $(document).ready(function (){
     $(".cell").css("background-color", "grey");
+    $(".Hcell").css("background-color", "grey");
     var arr = $.find("div[data-letter]");
     var alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
     $(arr).each(function(){
@@ -12,17 +14,29 @@ $(document).ready(function (){
 function changeName(event) {
     var idx = $(event).attr('placeholder');
     var arr = $.find("div[data-id='" + idx + "']");
+    var name = "";
+    if($(event).val()){
+            name = $(event).val();
+        }
+        else{
+             name = idx
+        }
 
     $(arr).each(function(){
         if($(event).val()){
-          $(this).text( $(event).val() )
+            $(this).text( $(event).val() )
         }
         else{
              $(this).text( idx )
         }
-
-    })
+    });
+    lands.find(function (element) {
+        if (element.id == idx){
+            element.name = name
+        }
+    });
 }
+
 
 function update(jscolor, event) {
 // 'jscolor' instance can be used as a string
@@ -31,4 +45,36 @@ function update(jscolor, event) {
 
     $(arr).css("background-color", '#' + jscolor);
 
+}
+
+
+function checkMaze(){
+    var incomplete = false;
+    lands.find(function (element) {
+        if (element.name == element.id || element.color == "808080"){
+            incomplete = true;
+        }
+    });
+    console.log(incomplete)
+    if(incomplete){
+
+        swal({
+          title: 'Are you sure?',
+          text: "You won't be able to revert this!",
+          type: 'warning',
+          showCancelButton: true,
+          confirmButtonColor: '#3085d6',
+          cancelButtonColor: '#d33',
+          confirmButtonText: 'Yes, delete it!'
+        }).then(function(result) {
+          if (result) {
+            swal(
+              'Deleted!',
+              'Your file has been deleted.',
+              'success'
+            )
+          }
+        })
+
+    }
 }
